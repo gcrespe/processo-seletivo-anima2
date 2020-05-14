e65efe41453365a62e25aa0877fa27f13c52536c
Giuliano Crespe <gcrespe3@hotmail.com>
2020-05-14 09:03:12 -0300

    Updates

---
+++ precache-manifest.5d5628286683bdbfe3d1dd8d7a97a8b4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4999728a77f9bf21141675c464b56a93",
+    "url": "/processo-seletivo-anima2/index.html"
+  },
+  {
+    "revision": "2b05dbf7714ecd72f4f6",
+    "url": "/processo-seletivo-anima2/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "99d7ba5982abdb831369",
+    "url": "/processo-seletivo-anima2/static/css/main.b7f70691.chunk.css"
+  },
+  {
+    "revision": "2b05dbf7714ecd72f4f6",
+    "url": "/processo-seletivo-anima2/static/js/2.5811842a.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/processo-seletivo-anima2/static/js/2.5811842a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "99d7ba5982abdb831369",
+    "url": "/processo-seletivo-anima2/static/js/main.eb01c1bb.chunk.js"
+  },
+  {
+    "revision": "1793b9c150ff484d7bcb",
+    "url": "/processo-seletivo-anima2/static/js/runtime-main.ebc526fc.js"
+  }
+]);
